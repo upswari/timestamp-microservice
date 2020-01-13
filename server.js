b6a4403a071c21514c8b0c1baf33180cdc661246
upswari <fbar615@gmail.com>
2020-01-13 14:17:43 +0700

    unix fix

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ app.get('/api/timestamp', (req, res) => {
 
 }).get('/api/timestamp/:date_string', (req, res, next) => {
     let date_string = req.params.date_string,
-        r_unix = /[0-9]{10}/,
+        r_unix = /[0-9]{13}/,
         r_utc = /\d{4}[-]\d{1,}[-]\d{2}/,
         valid_unix = date_string.match(r_unix) != null ? date_string.match(r_unix).map(x => x).toString() : null,
         valid_utc = date_string.match(r_utc) != null ? date_string.match(r_utc).map(x => x).toString() : null
 
     if (valid_unix != null && valid_unix == date_string) {
-        date_string = parseInt(date_string.concat('000'))
-        req.data = { "unix": new Date(date_string), "utc": new Date(date_string).toUTCString() }
+        date_string = parseInt(date_string)
+        req.data = { "unix": new Date(date_string).getTime(), "utc": new Date(date_string).toUTCString() }
         next()
     } else if (valid_utc != null && (new Date(date_string).toUTCString() != 'Invalid Date')) {
         req.data = { "unix": new Date(date_string).getTime(), "utc": new Date(date_string).toUTCString() }
